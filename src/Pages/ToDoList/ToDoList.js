efd384e2733a65f3c7c53ae67ec5bd1aa3fbbac2
raselcsedev <raselcse.dev@gmail.com>
2022-07-01 09:07:04 +0600

    todo list designed

--- src/Pages/ToDoList/ToDoList.js
+++ src/Pages/ToDoList/ToDoList.js
@@ -1,9 +1,73 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const ToDoList = () => {
+    const [todos, setTodos] = useState([
+        {
+            'title': 'First Todo',
+            'status': 'Pending'
+        },
+        {
+            'title': 'Second Todo',
+            'status': 'Done'
+        },
+        {
+            'title': 'Third Todo',
+            'status': 'Done'
+        },
+    ])
     return (
         <div>
-            <h1>Todo list</h1>
+            
+            <div class="overflow-x-auto w-full">
+                <table class="table w-full">
+                    {/*  <!-- head --> */}
+                    <thead>
+                        <tr>
+                            <th>
+                                <label></label>
+                            </th>
+                            <th>Title</th>
+                            <th>Details</th>
+                            <th></th>
+
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {/* <!-- row 1 --> */}
+                        {
+                todos.map((todo, index) => ( 
+                    <tr key={index}>
+                            <th>
+                                <label>
+                                    <input type="checkbox" class="checkbox" />
+                                </label>
+                            </th>
+                            <td>
+                                <div class="flex items-center space-x-3">
+                                    <div>
+                                        <div class="font-bold">{todo.title}</div>
+                                    </div>
+                                </div>
+                            </td>
+                            <td>
+                                {todo.status}
+                            </td>
+                            <td>
+                                <button class="btn mx-2 btn-outline btn-xs">edit</button>
+                                <button class="btn mx-2 btn-outline btn-xs">delete</button>
+                            </td>
+                        </tr>
+
+                ))
+            }
+
+                        
+                        {/* <!-- row 2 --> */}
+
+                    </tbody>
+
+                </table>
+            </div>
         </div>
     );
 };
